120b39d840b6ed84874a0ce8598b1c3297a3701b
ImKuroKy <spiritofthesovietunion@gmail.com>
2024-10-04 05:03:09 +0400

    feat: добавлены компоненты навигационной панели, домашней страницы и подвала
    - Добавлен компонент навигационной панели
    - Добавлен компонент домашней страницы с начальной разметкой и контентом
    - Добавлен компонент подвала
    - Включено изображение логотипа компании в ресурсы проекта
    - Удалены автоматически сгенерированные файлы тестов

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './featuries/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: "Главная",
+        path: "",
+        component: HomePageComponent,
+    },
+];
