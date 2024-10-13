8061258ddf10aa2051a88e4d10d13ec276e9ba0e
ImKuroKy <spiritofthesovietunion@gmail.com>
2024-10-13 14:40:10 +0400

    feat: добавление login-page и register-page
    - Добавлен новый компонент LoginPageComponent
    - Добавлен новый компонент RegisterPageComponent
    - Добавлена логика взаимодействия компонентов в app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,31 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './featuries/home-page/home-page.component';
+import { LoginPageComponent } from './featuries/login-page/login-page.component';
+import { RegisterPageComponent } from './featuries/register-page/register-page.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    {
-        title: "Главная",
-        path: "",
-        component: HomePageComponent,
-    },
+  {
+    title: 'Главная',
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    title: 'Вход',
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    title: 'Регистрация',
+    path: 'register',
+    component: RegisterPageComponent,
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }, // Redirect unknown routes to login
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ client/src/app/featuries/register-page/register-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [ RouterModule ],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent {
+
+}
