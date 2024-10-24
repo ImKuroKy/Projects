635db7f7a87a7bc34566e692deb9789759a2cafe
ImKuroKy <spiritofthesovietunion@gmail.com>
2024-10-24 05:01:27 +0400

    feat(register): Создана директива compare-password и добавлена логика компонента register-page
    - Создана и применена к полю подтверждения пароля новая директива compare-password.directive.
    - Добавлен импорт директивы compare-password.directive в app.component.
    - Внесены изменения в стили и разметку страницы, необходимые для реализации логики компонента register-page.
    - Добавлены проверки для полей никнейма, email и пароля на форме регистрации.
    - Реализована логика сравнения пароля и подтверждения пароля.
    - Обновлен компонент для отправки только необходимых данных на сервер (name, email, password).
    - Обработаны сценарии успешной регистрации и ошибок при регистрации.
    - Обновлены роуты в api.service в соответствии с роутами сервера.

--- client/src/app/featuries/register-page/register-page.component.ts
+++ client/src/app/featuries/register-page/register-page.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { ComparePasswordDirective } from '../../directives/compare-password.directive';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [CommonModule, FormsModule, ComparePasswordDirective],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
+  user = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
 
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      const { name, email, password } = this.user;
+      const requestData = { name, email, password };
+      this.apiService.postData(requestData).subscribe({
+        next: (response) => {
+          this.router.navigate(['/auth/login']);
+        },
+        error: (error) => {
+          console.error('Error registering user', error);
+        },
+      });
+    }
+  }
 }
--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -12,10 +12,10 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/data`);
+    return this.http.get<any>(`${this.apiUrl}auth/login`);
   }
 
   postData(data: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/data`, data);
+    return this.http.post<any>(`${this.apiUrl}auth/register`, data);
   }
 }
