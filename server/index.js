f3d66777e448eabd68e5047c156323ee7589b919
ImKuroKy <spiritofthesovietunion@gmail.com>
2024-10-24 17:57:39 +0400

    feat(server): Добавлена библиотека cors
    - Добавлена и подключена к приложению библиотека cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4040;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
