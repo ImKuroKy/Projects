41df57ead4da0f4cf6791813f2c746b3254013f8
ImKuroKy <spiritofthesovietunion@gmail.com>
2024-12-01 04:29:32 +0400

    fix(warehouse): Исправление роутов для запросов к складам
    - Изменена запросы к базе данных в модели `warehouse`.
    - Изменены и добавлены корректные методы для запросов к роутам в контроллере `warehouseController`.
    - Добавлен роут в роутере `warehouseRouter` для обработки запросов типа `/warehouse/:id`.

--- server/models/warehouse.js
+++ server/models/warehouse.js
@@ -1,24 +1,14 @@
 import pool from "../config/exports.js";
 
-// Получение информации о товарах на складе
 export const fetchWarehouseProducts = async (warehouseId) => {
   const query = `
     SELECT 
-        w.name AS warehouse_name,
-        wp.quantity,
-        p.vendor_code,
         p.name AS product_name,
+        wp.quantity,
         p.description,
         p.price,
-        p.picture,
         p.manufacturer,
-        s.name AS supplier_name,
-        sa.street AS supplier_street,
-        sa.city AS supplier_city,
-        sa.state AS supplier_state,
-        sa.postal_code AS supplier_postal_code,
-        sa.country AS supplier_country,
-        w.max_capacity
+        s.name AS supplier_name
     FROM 
         "warehouse-management".warehouse w
     JOIN 
@@ -27,17 +17,34 @@ export const fetchWarehouseProducts = async (warehouseId) => {
         "warehouse-management".product p ON wp.product_id = p.product_id
     LEFT JOIN 
         "warehouse-management".supplier s ON p.supplier_id = s.supplier_id
-    LEFT JOIN 
-        "warehouse-management".supplier_address sa ON s.address_id = sa.address_id
     WHERE 
         w.warehouse_id = $1;
   `;
 
-  try {
-    const result = await pool.query(query, [warehouseId]);
-    return result.rows;
-  } catch (err) {
-    console.error("Error fetching warehouse products:", err);
-    throw err;
+  const result = await pool.query(query, [warehouseId]);
+
+  return result.rows;
+};
+
+export const fetchWarehouseDetails = async (warehouseId) => {
+  const query = `
+    SELECT 
+        w.name,
+        CONCAT(wa.street, ', ', wa.city, ', ', wa.state, ', ', wa.postal_code, ', ', wa.country) AS address,
+        w.max_capacity AS capacity
+    FROM 
+        "warehouse-management".warehouse w
+    JOIN 
+        "warehouse-management".warehouse_address wa ON w.address_id = wa.address_id
+    WHERE 
+        w.warehouse_id = $1;
+  `;
+
+  const result = await pool.query(query, [warehouseId]);
+
+  if (result.rows.length === 0) {
+    return null;
   }
+
+  return result.rows[0];
 };
